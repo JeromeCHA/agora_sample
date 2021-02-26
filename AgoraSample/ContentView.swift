f39b8d4466338fec436a1b27306b18bbc9e1fcff
Jerome Cha <jerome@chatwork.com>
2021-02-26 10:50:10 +0900

    Initial Commit

---
+++ AgoraSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AgoraSample
+//
+//  Created by jerome cha on 2021/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
